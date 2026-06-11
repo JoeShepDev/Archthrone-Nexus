c4376d30d1ce5361a2052af69303640f10bbc436
Joseph <joseph.shepherd.e@gmail.com>
2026-06-11 18:42:42 -0400

    Weather API Calls

--- nexus/src/main/java/com/archthrone/nexus/controller/WeatherController.java
+++ nexus/src/main/java/com/archthrone/nexus/controller/WeatherController.java
@@ -4,14 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.archthrone.nexus.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 @RequestMapping("/api/weather")
 public class WeatherController 
 {
+    @Autowired
+    private WeatherService weatherService;
+
     @GetMapping
     public String getWeather(@RequestParam double lat, @RequestParam double lon) 
     {
-        return "Weather endpoint hit - lat: " + lat + " lon: " + lon;
+        return weatherService.getWeather(lat, lon);
     }
 }
--- nexus/src/main/java/com/archthrone/nexus/service/WeatherService.java
+++ nexus/src/main/java/com/archthrone/nexus/service/WeatherService.java
@@ -9,4 +9,13 @@ public class WeatherService
 {
     @Value("${weather.api.key}")
     private String _apiKey;
+
+    public String getWeather(Double lat, Double lon)
+    {
+        String url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + _apiKey + "&units=metric";
+
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(url, String.class);
+        return response;
+    }
 }
